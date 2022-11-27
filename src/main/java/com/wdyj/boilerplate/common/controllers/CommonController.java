12b204ddb1452d5b61723b24d9dfff9c6014aa02
ba912 <a921003@naver.com>
2022-11-27 21:22:38 +0900

    feat: common result / directory

--- src/main/java/com/wdyj/boilerplate/common/controllers/CommonController.java
+++ src/main/java/com/wdyj/boilerplate/common/controllers/CommonController.java
@@ -4,6 +4,7 @@ import com.wdyj.boilerplate.common.dto.CommonResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,6 +12,7 @@ import java.util.Objects;
 
 @RestController
 @Slf4j
+@RequestMapping("/")
 public class CommonController {
 
     @GetMapping("/heath")
